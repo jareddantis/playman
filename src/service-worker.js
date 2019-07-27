a93efccf855e78e584d6a4976b6b9171d5158f64
Jared Dantis <jareddantis@gmail.com>
2019-07-27 22:24:24 +0800

    sw.js: Cache Spotify images too

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,19 +1,19 @@
 /**
-    @file service-worker.js
-    @description Custom logic for the service worker.
-                 Handles Google Font and JS lib caching.
-    @author Jared Dantis (@jareddantis)
-    @license GPLv2
-*/
+ @file service-worker.js
+ @description Custom logic for the service worker.
+ Handles Google Font and JS lib caching.
+ @author Jared Dantis (@jareddantis)
+ @license GPLv2
+ */
 
 // Debug logs
 workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
 // Skip waiting
 self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-        self.skipWaiting();
-    }
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 // Precache
@@ -24,47 +24,53 @@ workbox.routing.registerNavigationRoute('/index.html');
 
 // Workbox plugins
 var newPlugins = function() {
-    return [
-        new workbox.cacheableResponse.Plugin({
-            statuses: [0, 200],
-        }),
-        new workbox.expiration.Plugin({
-            maxAgeSeconds: 60 * 60 * 24 * 365,
-            maxEntries: 30,
-        }),
-    ];
+  return [
+    new workbox.cacheableResponse.Plugin({
+      statuses: [0, 200],
+    }),
+    new workbox.expiration.Plugin({
+      maxAgeSeconds: 60 * 60 * 24 * 365,
+      maxEntries: 30,
+    }),
+  ];
 };
 // Stale-while-revalidate
 var newSwrStrategy = function (cacheName) {
-    return new workbox.strategies.StaleWhileRevalidate({
-        cacheName: cacheName,
-        plugins: newPlugins(),
-    });
+  return new workbox.strategies.StaleWhileRevalidate({
+    cacheName: cacheName,
+    plugins: newPlugins(),
+  });
 };
 // Cache-first
 var newCfStrategy = function (cacheName) {
-    return new workbox.strategies.CacheFirst({
-        cacheName: cacheName,
-        plugins: newPlugins(),
-    });
+  return new workbox.strategies.CacheFirst({
+    cacheName: cacheName,
+    plugins: newPlugins(),
+  });
 };
 
 // Cache files from Google API storage (e.g. Workbox scripts)
 workbox.routing.registerRoute(
-    /^https:\/\/storage\.googleapis\.com/,
-    newSwrStrategy('google-storage')
+  /^https:\/\/storage\.googleapis\.com/,
+  newSwrStrategy('google-storage')
 );
 
 // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
 // https://developers.google.com/web/tools/workbox/guides/common-recipes
 workbox.routing.registerRoute(
-    /^https:\/\/fonts\.googleapis\.com/,
-    newSwrStrategy('google-fonts-stylesheets')
+  /^https:\/\/fonts\.googleapis\.com/,
+  newSwrStrategy('google-fonts-stylesheets')
 );
 
 // Cache the underlying font files with a cache-first strategy for 1 year.
 // https://developers.google.com/web/tools/workbox/guides/common-recipes
 workbox.routing.registerRoute(
-    /^https:\/\/fonts\.gstatic\.com/,
-    newCfStrategy('google-fonts-webfonts')
+  /^https:\/\/fonts\.gstatic\.com/,
+  newCfStrategy('google-fonts-webfonts')
+);
+
+// Cache Spotify API playlist images.
+workbox.routing.registerRoute(
+  /^https:\/\/pl.scdn.co\/images/,
+  newSwrStrategy('spotify-playlist-img')
 );
