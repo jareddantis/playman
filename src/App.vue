492036a426520fffc1df37ec9d81e01e557e5cd6
Jared Dantis <jareddantis@gmail.com>
2019-07-28 21:02:04 +0800

    App: Separate styles, add avatar and username view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
+<!--    Navbar -->
+    <div id="navbar" v-show="isLoggedIn">
+      <span>{{ username }}</span>
+      <img :src="avatarUri" :alt="username" />
+    </div>
+
+<!--    Router view -->
     <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <style lang="stylus">
   @require './styles/global'
-
-  #app
-    width 100%
+  @require './styles/App'
 </style>
 
 <script lang="ts">
   import Vue from 'vue'
+  import { Component } from 'vue-property-decorator'
+  import { mapState } from 'vuex'
 
-  export default class App extends Vue {}
+  @Component({
+    computed: mapState(['avatarUri', 'isLoggedIn', 'username']),
+  })
+  export default class App extends Vue {
+    public avatarUri!: string
+    public isLoggedIn!: boolean
+    public username!: string
+  }
 </script>
