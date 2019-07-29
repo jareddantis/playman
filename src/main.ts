7cc52f4220504d78c30d5a1c00ea5a9aa922bba3
Jared Dantis <jareddantis@gmail.com>
2019-07-29 20:01:47 +0800

    main.ts: Import global styles

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './views'
 import eventBus from './plugins/eventBus'
 import store from './plugins/store'
 import './registerServiceWorker'
+import './styles/global.styl'
 
 Vue.use(eventBus)
 Vue.config.productionTip = false
