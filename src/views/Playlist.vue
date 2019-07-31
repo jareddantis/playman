ce5241e8ee05d0db9f09dc3c844a776501f2e4e3
Jared Dantis <jareddantis@gmail.com>
2019-07-31 22:57:31 +0800

    Playlist: Scroll to top on page change

--- src/views/Playlist.vue
+++ src/views/Playlist.vue
@@ -26,112 +26,117 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import { Component } from 'vue-property-decorator'
-  import PlaylistTrack from '@/components/PlaylistTrack.vue'
+import Vue from 'vue'
+import { Component, Watch } from 'vue-property-decorator'
+import PlaylistTrack from '@/components/PlaylistTrack.vue'
 
-  @Component({
-    components: { PlaylistTrack },
-  })
-  export default class Playlist extends Vue {
-    public page: number = 1
-    public pages: number = 1
-    public playlistName: string = ''
-    public playlistArt: string = ''
-    private allTracks: any[] = []
-    private checkedTracks: string[] = []
-    private readonly pageLimit: number = 50
+@Component({
+  components: { PlaylistTrack },
+})
+export default class Playlist extends Vue {
 
-    public mounted() {
-      this.$bus.$emit('loading', true)
-      this.getPlaylist().then(() => this.$bus.$emit('loading', false))
+  get id(): string {
+    return this.$route.params.id
+  }
+  get playlistTracks(): any[] {
+    const begin = (this.page - 1) * this.pageLimit
+    const end = begin + this.pageLimit
+    return this.allTracks.slice(begin, end)
+  }
+  public page: number = 1
+  public pages: number = 1
+  public playlistName: string = ''
+  public playlistArt: string = ''
+  private allTracks: any[] = []
+  private checkedTracks: string[] = []
+  private readonly pageLimit: number = 50
+
+  public mounted() {
+    this.$bus.$emit('loading', true)
+    this.getPlaylist().then(() => this.$bus.$emit('loading', false))
+  }
+
+  public onTrackToggled(payload: any) {
+    const { id, state } = payload
+
+    if (state) {
+      this.checkedTracks.push(id)
+    } else {
+      this.checkedTracks.splice(this.checkedTracks.indexOf(id), 1)
     }
 
-    public onTrackToggled(payload: any) {
-      const { id, state } = payload
-
-      if (state) {
-        this.checkedTracks.push(id)
-      } else {
-        this.checkedTracks.splice(this.checkedTracks.indexOf(id), 1)
-      }
-
-      // Update action bar
-      if (this.checkedTracks.length) {
-        this.$bus.$emit('change-navbar', {
-          actionBar: 'Tracks',
-          backButton: false,
-          name: '&nbsp;',
-        })
-      } else {
-        this.setInitialNavbar()
-      }
-    }
-
-    private setInitialNavbar() {
+    // Update action bar
+    if (this.checkedTracks.length) {
       this.$bus.$emit('change-navbar', {
-        actionBar: 'Playlist',
-        backButton: true,
-        name: this.playlistName,
+        actionBar: 'Tracks',
+        backButton: false,
+        name: '&nbsp;',
       })
-    }
-
-    private async getPlaylist() {
-      return new Promise((resolve, reject) => {
-        // Load playlist details
-        this.$store.dispatch('getPlaylist', this.id)
-          .then((playlist) => {
-            this.playlistArt = playlist.images[0].url
-            this.playlistName = playlist.name
-            this.pages = Math.ceil(playlist.tracks.total / this.pageLimit)
-            this.setInitialNavbar()
-          })
-          .catch((error) => reject(error))
-
-        // Load playlist tracks
-        this.$store.dispatch('getPlaylistTracks', this.id)
-          .then((response) => {
-            // Simplify track objects
-            // so we don't have to re-process everything when changing pages
-            for (const item of response) {
-              const { track } = item
-              this.allTracks.push({
-                id: track.id,
-                album: track.album.name,
-                artist: track.artists.length === 1 ? track.artists[0].name : this.generateArtists(track.artists),
-                name: track.name,
-              })
-            }
-
-            resolve()
-          })
-          .catch((error) => reject(error))
-      })
-    }
-
-    get id(): string {
-      return this.$route.params.id
-    }
-    get playlistTracks(): any[] {
-      const begin = (this.page - 1) * this.pageLimit
-      const end = begin + this.pageLimit
-      return this.allTracks.slice(begin, end)
-    }
-
-    private generateArtists(artists: any[]): string {
-      let str = ''
-
-      artists.forEach((artist, index) => {
-        str += artist.name
-
-        if (index < artists.length - 1) {
-          str += ', '
-        }
-      })
-
-      return str
+    } else {
+      this.setInitialNavbar()
     }
   }
+
+  @Watch('page')
+  private onPageChanged() {
+    window.scrollTo(0, 0)
+  }
+
+  private setInitialNavbar() {
+    this.$bus.$emit('change-navbar', {
+      actionBar: 'Playlist',
+      backButton: true,
+      name: this.playlistName,
+    })
+  }
+
+  private async getPlaylist() {
+    return new Promise((resolve, reject) => {
+      // Load playlist details
+      this.$store.dispatch('getPlaylist', this.id)
+        .then((playlist) => {
+          this.playlistArt = playlist.images[0].url
+          this.playlistName = playlist.name
+          this.pages = Math.ceil(playlist.tracks.total / this.pageLimit)
+          this.setInitialNavbar()
+        })
+        .catch((error) => reject(error))
+
+      // Load playlist tracks
+      this.$store.dispatch('getPlaylistTracks', this.id)
+        .then((response) => {
+          // Simplify track objects
+          // so we don't have to re-process everything when changing pages
+          for (const item of response) {
+            const { track } = item
+            this.allTracks.push({
+              id: track.id,
+              album: track.album.name,
+              artist: track.artists.length === 1 ? track.artists[0].name : this.generateArtists(track.artists),
+              name: track.name,
+            })
+          }
+
+          resolve()
+        })
+        .catch((error) => reject(error))
+    })
+  }
+
+  private generateArtists(artists: any[]): string {
+    let str = ''
+
+    artists.forEach((artist, index) => {
+      str += artist.name
+
+      if (index < artists.length - 1) {
+        str += ', '
+      }
+    })
+
+    return str
+  }
+}
 </script>
 
 <style lang="scss" scoped>
