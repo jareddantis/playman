c1826baebfa530fa09263d2b6ff1eba3faa51696
Jared Dantis <jareddantis@gmail.com>
2019-07-31 19:28:54 +0800

    components: Add action bar for Playlist view

---
+++ src/components/PlaylistBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="action-bar">
+    <v-btn text small icon color="white">
+      <v-icon>edit</v-icon>
+    </v-btn>
+    <v-btn text small icon color="white">
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import { Component } from 'vue-property-decorator'
+
+  @Component
+  export default class PlaylistBar extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  @import '../styles/components/ActionBar';
+</style>
