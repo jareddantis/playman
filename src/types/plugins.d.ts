eca7de778b4fa057d27f00ca910a39a0deaf93c8
Jared Dantis <jareddantis@gmail.com>
2019-08-05 16:54:08 +0800

    types: Update definitions for Spotify API wrapper

--- src/types/plugins.d.ts
+++ src/types/plugins.d.ts
@@ -5,14 +5,22 @@ declare class Spotify {
   public authenticated: boolean
   public redirectUri: string
 
-  public deletePlaylistTracks(id: string, tracks: any[], snapshot: string,
+  public changePlaylistDetails(id: string, details: any): Promise<any>
+  public deleteAllPlaylistTracks(id: string): Promise<Promise<any>>
+  public deletePlaylistTracks(id: string, initial: any[], snapshot: string,
                               resolve: (arg0: any) => void, reject: (arg0: any) => void): Promise<any>
-  public getMe(): Promise<any>
+  public getMe(): Promise<void>
   public getPlaylist(id: string): Promise<any>
-  public getPlaylistTracks(id: string, initial: any[], offset: number,
-                           resolve: (arg0: any) => void, reject: (arg0: any) => void): Promise<any>
-  public getUserPlaylists(username: string): Promise<any>
+  public getUserPlaylists(username: string, initial: any[],
+                          resolve: (arg0: any) => void, reject: (arg0: any) => void): Promise<void>
+  public reorderPlaylistTracks(id: string, snapshot: string, tracks: any[],
+                               tracksToReorder: number[], placeTracksAfter: number): Promise<any>
   public setTokens(access: string, refresh: string, expiry: number): void
+  public shufflePlaylist(id: string, snapshot: string, tracks: any[]): Promise<any>
+}
+
+declare module 'spotify' {
+  export = Spotify
 }
 
 declare module 'vue/types/vue' {
