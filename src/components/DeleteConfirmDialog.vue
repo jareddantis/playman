d2daab3ef38b4c90f29164ae621b0d90788bc835
Jared Dantis <jareddantis@gmail.com>
2019-08-05 20:49:30 +0800

    components: Add generic delete confirmation dialog

---
+++ src/components/DeleteConfirmDialog.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-dialog max-width="600" v-model="showDialog">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Delete selected {{ items }}?</span>
+      </v-card-title>
+      <v-card-text>
+        <p v-if="items === 'songs'"
+           class="body-1">This process is irreversible unless you make a backup first.</p>
+        <p v-else
+           class="body-1">You can always restore deleted playlists from your Spotify account settings.</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="showDialog = false" text>cancel</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn @click="confirm" text>delete</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
+
+@Component
+export default class DeleteConfirmDialog extends Vue {
+  public items: string = ''
+  public showDialog: boolean = false
+
+  public created() {
+    this.$bus.$on('delete-tracks', () => {
+      this.items = 'songs'
+      this.showDialog = true
+    })
+    this.$bus.$on('delete-playlists', () => {
+      this.items = 'playlists'
+      this.showDialog = true
+    })
+    this.$bus.$on('delete-playlist', () => {
+      this.items = 'playlist'
+      this.showDialog = true
+    })
+  }
+
+  public confirm() {
+    this.$emit('confirm')
+    this.showDialog = false
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
