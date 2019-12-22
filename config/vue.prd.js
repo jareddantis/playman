57282f678253b6e115af49d5e20a7fdbc9250846
Jared Dantis <jareddantis@gmail.com>
2019-12-22 13:18:27 +0800

    Update packages

--- config/vue.prd.js
+++ config/vue.prd.js
@@ -11,7 +11,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "~@/styles/overrides.sass"`,
+        prependData: `@import "~@/styles/overrides.sass"`,
       },
     },
   },
@@ -19,7 +19,7 @@ module.exports = {
   chainWebpack: (config) => {
     ["vue-modules", "vue", "normal-modules", "normal"].forEach((match) => {
       config.module.rule('scss').oneOf(match).use('sass-loader')
-        .tap((opt) => Object.assign(opt, { data: `@import '~@/styles/overrides.sass';` }))
+        .tap((opt) => Object.assign(opt, { prependData: `@import '~@/styles/overrides.sass';` }))
     })
   },
 
